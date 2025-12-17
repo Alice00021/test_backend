bc4dee9f091557d7922080516ec95c829170300f
Alice00021 <alisa_barysheva03@mail.ru>
2025-12-17 13:27:57 +0300

    add CORS

--- internal/controller/http/router.go
+++ internal/controller/http/router.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"github.com/Alice00021/test_common/pkg/logger"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
@@ -27,6 +28,8 @@ func NewRouter(handler *gin.Engine, cfg *config.Config, l logger.Interface, uc *
 	handler.Use(gin.Logger())
 	handler.Use(gin.Recovery())
 
+	handler.Use(cors.Default())
+
 	// K8s probe
 	handler.GET("/healthz", func(c *gin.Context) { c.Status(http.StatusOK) })
 
@@ -47,11 +50,12 @@ func NewRouter(handler *gin.Engine, cfg *config.Config, l logger.Interface, uc *
 	}
 
 	privateV1Group := handler.Group("/v1")
+	v1.NewBookRoutes(privateV1Group, l, uc.Book)
+
 	privateV1Group.Use(middleware.JwtAuthMiddleware(uc.Auth))
 	{
 		v1.NewUserRoutes(privateV1Group, l, uc.User)
 		v1.NewExportRoutes(privateV1Group, l, uc.Export)
-		v1.NewBookRoutes(privateV1Group, l, uc.Book)
 		v1.NewAuthorRoutes(privateV1Group, l, uc.Author)
 		v1.NewCommandRoutes(privateV1Group, l, uc.Command)
 		v1.NewOperationRoutes(privateV1Group, l, uc.OperationMongo)
